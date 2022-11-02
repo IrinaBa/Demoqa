18ba1ad5e1806d93ed020f743820a65d5a6e13c0
Iryna Bartz <1208ein@mail.ru>
2022-11-02 22:19:27 +0100

    Added 02.11

--- src/main/java/com/telran/data/UserData.java
+++ src/main/java/com/telran/data/UserData.java
@@ -4,4 +4,5 @@ public class UserData {
 
     public static final String USER_NAME = "IrinaB";
     public static final String USER_PASSWORD = "Irina120882!";
+    public static final String USER_EMAIL = "ira@gmail.com";
 }
